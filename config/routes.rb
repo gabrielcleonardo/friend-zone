7922027eccbbac2198eb61ecbe84efca66983280
Gabriel Leonardo <gabrielcleonardo@hotmail.com>
2021-08-24 17:12:19 -0300

    explore route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'explore', to: 'users#index'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # namespace :explore do
+  #   resources :users, only: [:index]
+  # end
 end
