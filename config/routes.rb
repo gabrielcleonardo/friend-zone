d8d430cd185a07e041c8f089157d664dcc8fda7a
Gabriel Leonardo <gabrielcleonardo@hotmail.com>
2021-08-25 12:04:12 -0300

    user/:id backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'explore', to: 'users#index'
+  get 'user/:id', to: 'users#show', as: :user
   devise_for :users
   root to: 'pages#home'
   # namespace :explore do
