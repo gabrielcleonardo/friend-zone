2d4da538a124d65c11ba31036f587ffdb02f3a3f
rafael boas <rafaelrvb@gmail.com>
2021-08-25 20:33:37 -0300

    quase pronto

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
   get 'explore', to: 'users#index'
   get 'user/:id', to: 'users#show', as: :user
+  get 'explore_map', to: 'users#show_map', as: :show_map
+  get 'my_matches', to: 'matches#index'
+  # devise:
   devise_for :users
   root to: 'pages#home'
-  get 'my_matches', to: 'matches#index'
+
 
 end
