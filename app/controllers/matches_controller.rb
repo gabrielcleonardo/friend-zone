3030bc226f5a36a03ff43c281f76f2df649886c0
carlos machado <carlosmachado05@hotmail.com>
2021-08-26 22:22:31 +0200

    match logic until my matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,18 @@
 class MatchesController < ApplicationController
   def index
-    @matches = Match.all
-    @user = current_user
+
+    @matches = Match.search_by_matches(current_user.id)
+
+  end
+
+  def create
+    @user_id_1 = current_user.id
+    @user_id_2 = User.find(params[:data]).id
+    @match = Match.new(user_1_id: @user_id_1, user_2_id: @user_id_2)
+    @match.save
+    redirect_to my_matches_path, notice: 'Congratulation, now wait for your match.'
+  end
+
+  def update
   end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,9 @@
 class Match < ApplicationRecord
   belongs_to :user_1, class_name: 'User'
   belongs_to :user_2, class_name: 'User'
+
+  # search
+  include PgSearch::Model
+  pg_search_scope :search_by_matches,
+  against: [ :user_1_id, :user_2_id ]
 end
