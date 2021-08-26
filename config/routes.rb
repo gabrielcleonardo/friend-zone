3e82cabd78f4b4e3e17b42f7b8664197a6f47d35
carlos machado <carlosmachado05@hotmail.com>
2021-08-26 23:36:11 +0200

    we fixed the indentation of index users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'explore_map', to: 'users#show_map', as: :show_map
   get 'my_matches', to: 'matches#index'
   post 'my_matches', to: 'matches#create', as: :create
+  patch 'my_matches', to: 'matches#update'
+  delete 'my_matches', to: 'matches#destroy'
   # devise:
   devise_for :users
   root to: 'pages#home'
